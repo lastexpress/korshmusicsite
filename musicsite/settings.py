3467e56a7f7a12abc51a2da6955d54390e575ec2
lastexpress <mama_ama2@mail.ru>
2022-03-04 19:19:59 +0400

    requrim

--- musicsite/settings.py
+++ musicsite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "application/static",
+    BASE_DIR / "static_cdn",
 ]
 
 STATIC_URL = '/static/'
---
+++ static_cdn/README.md
@@ -0,0 +1,1 @@
+online example number one !))
